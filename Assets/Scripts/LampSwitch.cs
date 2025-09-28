c07d57a98bbe197c2b39dc40e7d242699e4ce35e
EliasWickander <94359712+EliasWickander@users.noreply.github.com>
2025-09-28 19:11:11 +0200

    Fix broken origin

--- Assets/Scripts/LampSwitch.cs
+++ Assets/Scripts/LampSwitch.cs
@@ -15,11 +15,6 @@ public class LampSwitch : MonoBehaviour, IInteractable
 
     private ESwitchState m_currentState;
     public ESwitchState CurrentState => m_currentState;
-    
-    //AUDIO - adds a reference to the action in my script
-    [SerializeField] private AudioClip m_turnOnLamp;
-    [SerializeField] private AudioClip m_turnOffLamp;
-    [SerializeField] private InputActionReference toggleAction;
 
     private bool m_pendingState = false;
     private bool m_isOn = false;
@@ -27,28 +22,6 @@ public class LampSwitch : MonoBehaviour, IInteractable
     private int m_switchOffHash = Animator.StringToHash("SwitchOff");
     
     public event Action<bool> OnSwitchStateChanged;
-
-    //AUDIO - Subscribes to the Input Action in my class
-    private void OnEnable()
-    {
-        // Subscribe safely
-        if (toggleAction != null)
-        {
-            // Wait until the action is initialized
-            toggleAction.action.Enable();
-            toggleAction.action.performed += OnTogglePerformed; // use a named method
-        }
-    }
-
-    private void OnDisable()
-    {
-        if (toggleAction != null && toggleAction.action != null)
-        toggleAction.action.performed -= OnTogglePerformed;
-    }
-        private void OnTogglePerformed(InputAction.CallbackContext context)
-    {
-        Interact(); //calls my toggle logic once
-    }
 
     private void Start()
     {
@@ -65,24 +38,13 @@ public class LampSwitch : MonoBehaviour, IInteractable
     {
         return m_pendingState == m_isOn;
     }
-
+    
     private void Toggle(bool value)
     {
         m_pendingState = value;
         m_animator.SetTrigger(value ? m_switchOnHash : m_switchOffHash);
-
-        //AUDIO - Play click sound via SoundFXManager
-        //Only do anything if the state is changing
-        if (value == m_isOn)
-            return;
-        //Plays click sound via SoundFXManager
-        if (value)
-            SoundFXManager.instance.PlaySoundFXClip(m_turnOnLamp, transform, 1f);
-        else
-            SoundFXManager.instance.PlaySoundFXClip(m_turnOffLamp, transform, 1f);
-            // Updates the actual state
-            m_isOn = value;
     }
+    
     private void Update()
     {
         if (m_pendingState != m_isOn)
@@ -96,16 +58,6 @@ public class LampSwitch : MonoBehaviour, IInteractable
                 }
             }
         }
-    }
-        private void SetOnState(bool value)
-    {
-        return m_pendingState != true;
-    }
-
-    private void Toggle(bool value)
-    {
-        m_pendingState = value;
-        m_animator.SetTrigger(value ? m_switchOnHash : m_switchOffHash);
     }
     
     private void SetOnState(bool value)
--- Assets/Scripts/Light/Flashlight.cs
+++ Assets/Scripts/Light/Flashlight.cs
@@ -2,5 +2,16 @@ using UnityEngine;
 
 public class Flashlight : LightSource
 {
+    [SerializeField] private AudioClip m_turnOnLamp;
+    [SerializeField] private AudioClip m_turnOffLamp;
 
+    protected override void OnToggle(bool isOn)
+    {
+        base.OnToggle(isOn);
+
+        if (isOn)
+            SoundFXManager.instance.PlaySoundFXClip(m_turnOnLamp, transform, 1f);
+        else
+            SoundFXManager.instance.PlaySoundFXClip(m_turnOffLamp, transform, 1f);
+    }
 }
